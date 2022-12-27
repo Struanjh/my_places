eaf57ecce9a83ca8e759c362a1245026f23025bc
STRUAN HENDERSON <Struanjh@gmail.com>
2022-12-27 12:59:30 +0000

    CSS Grid Layout for list page

--- views/list.php
+++ views/list.php
@@ -5,14 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/styles/index.css">
+    <link rel="stylesheet" href="public/styles/list.css">
     <script src="public/scripts/functions.js" type="module" defer></script>
     <script src="public/scripts/list.js" type="module" defer></script>
     <title>List Restaurants</title>
 </head>
 <body>
+    <a href="index.php?action=add-restaurant">Back to add page</a>
+    <h1 class="results-header">Here are all the aviailable restaurants: </h1>
     <div id="results-container">
-        <a href="index.php?action=add-restaurant">Back to add page</a>
-        <h1>Here are all the aviailable restaurants: </h1>
         <?php
         for ($i=0; $i<count($listOfRestaurants); $i++) {
         ?>
